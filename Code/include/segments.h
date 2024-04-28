4f2c78090a0d302769efb4bad4fee805e871a6ab
Carl Raymond <carl@orcharddev.com>
2024-04-28 18:01:13 -0400

    Fix segment order

--- Code/include/segments.h
+++ Code/include/segments.h
@@ -1,54 +1,56 @@
 #include <Arduino.h>
+
 // Seven-segment lookup table
 //      MSB                LSB
-//      _  G  F  E  D  C  B  A
-// 0:   0  0  1  1  1  1  1  1
-const byte SEGMENTS_0 = 0x37;
+//      _  A  B  C  D  E  F  G
 
-// 1:   0  0  0  0  0  1  1  0
-const byte SEGMENTS_1 = 0x06;
+// 0:   0  1  1  1  1  1  1  0
+const byte SEGMENTS_0 = 0x7e;
 
-// 2:   0  1  0  1  1  0  1  1
-const byte SEGMENTS_2 = 0x5b;
+// 1:   0  0  1  1  0  0  0  0
+const byte SEGMENTS_1 = 0x30;
 
-// 3:   0  1  0  0  1  1  1  1
-const byte SEGMENTS_3 = 0x4f;
+// 2:   0  1  1  0  1  1  0  1
+const byte SEGMENTS_2 = 0x6d;
 
-// 4:   0  1  1  0  0  1  1  0
-const byte SEGMENTS_4 = 0x66;
+// 3:   0  1  1  1  1  0  0  1
+const byte SEGMENTS_3 = 0x79;
 
-// 5:   0  1  1  0  1  1  0  1
-const byte SEGMENTS_5 = 0x6d;
+// 4:   0  0  1  1  0  0  1  1
+const byte SEGMENTS_4 = 0x33;
 
-// 6:   0  1  1  1  1  1  0  1
-const byte SEGMENTS_6 = 0x7d;
+// 5:   0  1  0  1  1  0  1  1
+const byte SEGMENTS_5 = 0x5b;
 
-// 7:   0  0  0  0  0  1  1  1
-const byte SEGMENTS_7 = 0x07;
+// 6:   0  1  0  1  1  1  1  1
+const byte SEGMENTS_6 = 0x5f;
+
+// 7:   0  1  1  1  0  0  0  0
+const byte SEGMENTS_7 = 0x70;
 
 // 8:   0  1  1  1  1  1  1  1
 const byte SEGMENTS_8 = 0x7f;
 
-// 9:   0  1  1  0  1  1  1  1
-const byte SEGMENTS_9 = 0x6f;
+// 9:   0  1  1  1  1  0  1  1
+const byte SEGMENTS_9 = 0x7b;
 
 // A:   0  1  1  1  0  1  1  1
 const byte SEGMENTS_A = 0x77;
 
-// B:   0  1  1  1  1  1  0  0
-const byte SEGMENTS_B = 0x7c;
+// B:   0  0  0  1  1  1  1  1
+const byte SEGMENTS_B = 0x1f;
 
-// C:   0  0  1  1  1  0  0  1
-const byte SEGMENTS_C = 0x39;
+// C:   0  1  0  0  1  1  1  0
+const byte SEGMENTS_C = 0x4e;
 
-// D:   0  1  0  1  1  1  1  0
-const byte SEGMENTS_D = 0x5e;
+// D:   0  0  1  1  1  1  0  1
+const byte SEGMENTS_D = 0x3d;
 
-// E:   0  1  1  1  1  0  0  1
-const byte SEGMENTS_E = 0x79;
+// E:   0  1  0  0  1  1  1  1
+const byte SEGMENTS_E = 0x4f;
 
-// F:   0  1  1  1  0  0  0  1
-const byte SEGMENTS_F = 0x71;
+// F:   0  1  0  0  0  1  1  1
+const byte SEGMENTS_F = 0x47;
 
 const byte SEGMENTS[] = {
     SEGMENTS_0, SEGMENTS_1, SEGMENTS_2, SEGMENTS_3, SEGMENTS_4, SEGMENTS_5, SEGMENTS_6, SEGMENTS_7,
